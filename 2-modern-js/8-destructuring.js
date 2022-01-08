52c1c1650e279bf4e767a6e34c0a461cbbb85256
Yeonjoon Choi <yeonjoonchoi@RBXMNPC0249.local>
2022-01-08 23:53:55 +0900

    testing how to create pull request

--- 2-modern-js/8-destructuring.js
+++ 2-modern-js/8-destructuring.js
@@ -4,18 +4,18 @@
 
 const { PI, E, SQRT2 }  = Math;
 
-// With require
-// const { readFile } = require('fs');
+With require
+const { readFile } = require('fs');
 
 
-// const circle = {
-//   label: 'circleX',
-//   radius: 2,
-// };
-//
-// const circleArea = ({ radius }) =>
-//   (PI * radius * radius).toFixed(2);
-//
-// console.log(
-//   circleArea(circle)
-// );
+const circle = {
+  label: 'circleX',
+  radius: 2,
+};
+
+const circleArea = ({ radius }) =>
+  (PI * radius * radius).toFixed(2);
+
+console.log(
+  circleArea(circle)
+);
