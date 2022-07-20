7f0c70f1527e8efe9ea4f82434617d846b7e2093
Laura Brown <laura.brown@hymans.co.uk>
2022-07-20 15:45:42 +0100

    destructuring

--- 2-modern-js/8-destructuring.js
+++ 2-modern-js/8-destructuring.js
@@ -8,14 +8,20 @@ const { PI, E, SQRT2 }  = Math;
 // const { readFile } = require('fs');
 
 
-// const circle = {
-//   label: 'circleX',
-//   radius: 2,
-// };
-//
-// const circleArea = ({ radius }) =>
-//   (PI * radius * radius).toFixed(2);
-//
-// console.log(
-//   circleArea(circle)
-// );
+const circle = {
+  label: 'circleX',
+  radius: 2,
+};
+
+const circleArea = ({ radius }, {precision = 2} = {}) =>
+  (PI * radius * radius).toFixed(precision);
+
+console.log(
+  circleArea(circle)
+);
+
+console.log(
+    circleArea(circle, { precision: 5})
+)
+
+const [first, second,,forth] = [10,20,30,40];
