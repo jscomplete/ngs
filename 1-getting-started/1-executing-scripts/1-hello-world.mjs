68bf4ca8a547c7b2c831ccbbba7759de8aed3b64
Laura Brown <laura.brown@hymans.co.uk>
2022-07-20 12:27:29 +0100

    Tutorial1

--- 1-getting-started/1-executing-scripts/1-hello-world.mjs
+++ 1-getting-started/1-executing-scripts/1-hello-world.mjs
@@ -1,6 +1,6 @@
-const http = require('http');
+import { createServer } from 'http';
 
-const server = http.createServer((req, res) => {
+const server = createServer((req, res) => {
   res.end('Laura\n');
 });
 
