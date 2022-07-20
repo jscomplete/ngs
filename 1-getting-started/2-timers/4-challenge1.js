216cf3d975c87ac52ca9362d2b256dc5ef52c6b3
Laura Brown <laura.brown@hymans.co.uk>
2022-07-20 13:06:33 +0100

    4 challenge no 1

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,9 +1,19 @@
-const theOneFunc = () => {};
+const theOneFunc = delay => {
+    
+    console.log ('Hello after ' + delay + ' seconds');
+};
 
-setTimeout(theOneFunc, 4 * 1000);
+setTimeout(theOneFunc, 4 * 1000, 4);
+setTimeout(theOneFunc, 8 * 1000, 8);
 
 // Hello after 4 seconds
 
 // Hello after 8 seconds
 
 // You can define only ONE function
+
+// const func = () => {
+//     console.log('Hello after 4 seconds');
+//   };
+  
+//   setTimeout(func, 4 * 1000);
