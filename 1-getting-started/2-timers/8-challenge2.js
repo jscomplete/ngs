971497a8e8c631e4c58d7265c1cbb531f4a1dc3a
Laura Brown <laura.brown@hymans.co.uk>
2022-07-20 13:33:00 +0100

    Challenge 8

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,11 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+
+for (let i = 0; i < 6; i++) {
+    console.log ('Hello World');
+    if (i == 5){
+    console.log ('Done');
+    }
+  }
