66520b6ec8c3f8ec7c51e8b1e44619b413971cf9
Laura Brown <laura.brown@hymans.co.uk>
2022-07-20 15:04:42 +0100

    Objects plus outputs

--- 2-modern-js/7-objects.js
+++ 2-modern-js/7-objects.js
@@ -1,6 +1,17 @@
+const mystery = "answer";
+
 const obj = {
   p1: 10,
   p2: 20,
   f1() {},
   f2: () => {},
+  [mystery]: 42,
 };
+
+//object will have a property answer with the value 42.
+// obj.answer
+
+// obj.mystery
+// undefined
+// obj.answer
+// 42
